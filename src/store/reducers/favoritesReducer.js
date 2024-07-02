439d93feacd99a95786ec557aea3b771a862314c
alicansadeler <alicansadeler@gmail.com>
2024-07-02 13:56:28 +0300

    favoritesReducer eklendi

--- src/store/reducers/favoritesReducer.js
+++ src/store/reducers/favoritesReducer.js
@@ -1,12 +1,36 @@
 import movies from "../../data";
+import {
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+  TOGGLE_FAVORITES,
+} from "../actions/favoritesActions";
 
 const initialState = {
-  favorites: movies,
+  favorites: [],
   displayFavorites: true,
 };
 
 export const favoritesReducer = (state = initialState, action) => {
   switch (action.type) {
+    case TOGGLE_FAVORITES:
+      return {
+        ...state,
+        displayFavorites: !state.displayFavorites,
+      };
+
+    case ADD_FAVORITE:
+      const fav = state.favorites.find((item) => item.id === action.payload.id);
+      if (fav) {
+        return state;
+      } else {
+        return { ...state, favorites: [...state.favorites, action.payload] };
+      }
+
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        favorites: state.favorites.filter((item) => item.id !== action.payload),
+      };
     default:
       return state;
   }
